b8c0dfc61f8693a68564abc02f9bdbc67ff153f9
Alexander W Blocker <ablocker@gmail.com>
2012-05-31 13:00:11 -0400

    Harmonize variable names; y_hat, p_int_cen, and p_rnd_cen.

--- src/lib.py
+++ src/lib.py
@@ -101,13 +101,13 @@ def dt(x, mu=0., scale=1., df=1., log=False):
         return ld
     return np.exp(ld)
 
-def densityratio(x, eta_0, eta_1, mu, sigmasq, approx_sd, yhat, propDf,
+def densityratio(x, eta_0, eta_1, mu, sigmasq, approx_sd, y_hat, propDf,
                  normalizing_cnst, log=False):
     '''
     Target-proposal ratio for censored intensity rejection sampler.
     '''
     ld = dcensored(x, mu, sigmasq, eta_0, eta_1, log=True)
-    ld -= dt(x, mu=yhat, scale=approx_sd, df=propDf, log=True)
+    ld -= dt(x, mu=y_hat, scale=approx_sd, df=propDf, log=True)
     ld += np.log(normalizing_cnst)
     if log:
         return ld
@@ -135,63 +135,63 @@ def deriv3_logdcensored(x, mu, sigmasq, eta_0, eta_1):
               (1. + np.exp(eta_0 + eta_1*x))**2)
     return deriv3
 
-def deriv_logdensityratio(x, eta_0, eta_1, mu, sigmasq, approx_sd, yhat,
+def deriv_logdensityratio(x, eta_0, eta_1, mu, sigmasq, approx_sd, y_hat,
                           propDf):
     '''
     First derivative of the log target-proposal ratio for censored intensity
     rejection sampler.
     '''
     deriv = deriv_logdcensored(x, mu, sigmasq, eta_0, eta_1)
-    deriv -= deriv_logdt(x, mu=yhat, scale=approx_sd, df=propDf)
+    deriv -= deriv_logdt(x, mu=y_hat, scale=approx_sd, df=propDf)
     return deriv
 
 # RNGs
 
-def rncen(nobs, prc, pic, lmbda, r):
+def rncen(n_obs, p_rnd_cen, p_int_cen, lmbda, r):
     '''
     Draw ncen | y, censoring probabilities, lambda, r.
-    Must have nObs, prc, and pic as Numpy vectors of same length.
+    Must have n_obs, p_rnd_cen, and p_int_cen as Numpy vectors of same length.
     r and lmbda must be scalars.
     '''
-    m = nobs.size
+    m = n_obs.size
 
     # Setup vectors for result and stopping indicators
     active  = np.ones(m, dtype=bool)
-    nCen    = np.zeros(m, dtype=int)
+    n_cen    = np.zeros(m, dtype=int)
 
     # Compute probability for geometric component of density
-    pgeom   = 1-(1-lmbda)*(prc+(1-prc)*pic)
+    pgeom   = 1-(1-lmbda)*(p_rnd_cen+(1-p_rnd_cen)*p_int_cen)
 
     # Compute necessary bound for envelope condition
     bound = np.ones(m)
     if r < 1:
-        bound *= (nobs + r - 1) / nobs
+        bound *= (n_obs + r - 1) / n_obs
 
     # Run rejection sampling iterations
     nIter = 0
     while np.sum(active) > 0:
         # Propose from negative binomial distribution
         # This is almost correct, modulo the 0 vs. 1 minimum non-conjugacy
-        prop    = np.random.negative_binomial(nobs[active] + r, pgeom[active],
+        prop    = np.random.negative_binomial(n_obs[active] + r, pgeom[active],
                                               size=active.size)
 
         # Compute acceptance probability; bog standard
         u       = np.random.uniform(size=np.sum(active))
-        pAccept = (nobs[active]+prop) / (nobs[active]+prop+r-1) * bound[active]
+        pAccept = (n_obs[active]+prop) / (n_obs[active]+prop+r-1)*bound[active]
 
-        # Alway accept for nObs == 0; in that case, our draw is exact
-        pAccept[nobs[active]==0] = 1.0
+        # Alway accept for n_obs == 0; in that case, our draw is exact
+        pAccept[n_obs[active]==0] = 1.0
 
         # Execute acceptance step and update done indicators
-        nCen[active[u<pAccept]] = prop[u<pAccept]
+        n_cen[active[u<pAccept]] = prop[u<pAccept]
         active[active] = u>pAccept
 
         nIter += 1
 
     # Add one to draws for nObs == 0; needed to meet constraint that all
     # peptides exist in at least one state.
-    nCen = nCen + (nobs==0)
-    return nCen
+    n_cen = n_cen + (n_obs==0)
+    return n_cen
 
 # Optimization and root-finding routines
 
@@ -301,7 +301,7 @@ def characterizeCensoredIntensityDist(eta_0, eta_1, mu, sigmasq,
     approximation.
 
     Returns dictionary with three entries:
-        1) yhat, the approximate mode of the given conditional distribution
+        1) y_hat, the approximate mode of the given conditional distribution
         2) p_int_cen, the approximate probabilities of intensity-based
            censoring
         3) approx_sd, the approximate SDs of the conditional intensity
@@ -317,40 +317,40 @@ def characterizeCensoredIntensityDist(eta_0, eta_1, mu, sigmasq,
     
     # First, start with a bit of bisection to get in basin of attraction for
     # Halley's method
-    yhat = vectorizedBisection(f=deriv_logdcensored, f_kwargs=dargs,
-                               lower=mu-bisectScale*np.sqrt(sigmasq),
-                               upper=mu+bisectScale*np.sqrt(sigmasq),
-                               tol=np.sqrt(tol), maxIter=bisectIter)
+    y_hat = vectorizedBisection(f=deriv_logdcensored, f_kwargs=dargs,
+                                lower=mu-bisectScale*np.sqrt(sigmasq),
+                                upper=mu+bisectScale*np.sqrt(sigmasq),
+                                tol=np.sqrt(tol), maxIter=bisectIter)
     
     # Second, run Halley's method to find the censored intensity distribution's
     # mode to much higher precision.
-    yhat = halley(f=deriv_logdcensored, fprime=deriv2_logdcensored,
-                  f2prime=deriv3_logdcensored, f_kwargs=dargs,
-                  x0=yhat, tol=tol, maxIter=maxIter)
+    y_hat = halley(f=deriv_logdcensored, fprime=deriv2_logdcensored,
+                   f2prime=deriv3_logdcensored, f_kwargs=dargs,
+                   x0=y_hat, tol=tol, maxIter=maxIter)
     
     # 2) Compute approximate SD of censored intensity distribution
-    info        = -deriv2_logdcensored(yhat, **dargs)
+    info        = -deriv2_logdcensored(y_hat, **dargs)
     approx_sd   = np.sqrt(1./info)
     
     # 3) Use Laplace approximation to approximate p(int. censoring); this is the
     # normalizing constant of the given conditional distribution
-    p_int_cen = laplaceApprox(f=dcensored, xhat=yhat, info=info,
+    p_int_cen = laplaceApprox(f=dcensored, xhat=y_hat, info=info,
                               f_kwargs=dargs)
     
     # Return dictionary containing combined result
-    result = {'yhat' : yhat,
+    result = {'y_hat' : y_hat,
               'p_int_cen' : p_int_cen,
               'approx_sd' : approx_sd}
     return result
     
-def boundDensityRatio(eta_0, eta_1, mu, sigmasq, yhat, approx_sd, propDf,
+def boundDensityRatio(eta_0, eta_1, mu, sigmasq, y_hat, approx_sd, propDf,
                       normalizing_cnst, tol=1e-10, maxIter=100,
                       bisectScale=1.):
     '''
     Bound ratio of t proposal density to actual censored intensity density.
     This is used to construct an efficient, robust rejection sampler to exactly
     draw from the conditional posterior of censored intensities.
-    This computation is fully vectorized with respect to mu, sigmasq, yhat,
+    This computation is fully vectorized with respect to mu, sigmasq, y_hat,
     approx_sd, and normalizing_cnst.
 
     Based on the properties of these two densities, their ratio will have three
@@ -366,20 +366,20 @@ def boundDensityRatio(eta_0, eta_1, mu, sigmasq, yhat, approx_sd, propDf,
              'mu' : mu,
              'sigmasq' : sigmasq,
              'approx_sd' : approx_sd,
-             'yhat' : yhat,
+             'y_hat' : y_hat,
              'propDf' : propDf}
     
     # Initialize vectors for all four of the bounds
-    left_lower = np.zeros_like(yhat)
-    left_upper = np.zeros_like(yhat)
-    right_lower = np.zeros_like(yhat)
-    right_upper = np.zeros_like(yhat)
+    left_lower = np.zeros_like(y_hat)
+    left_upper = np.zeros_like(y_hat)
+    right_lower = np.zeros_like(y_hat)
+    right_upper = np.zeros_like(y_hat)
     
     # Make sure the starting points are the correct sign
-    left_lower = yhat - bisectScale*approx_sd
-    left_upper = yhat - 10*tol
-    right_lower = yhat + 10*tol
-    right_upper = yhat + bisectScale*approx_sd
+    left_lower = y_hat - bisectScale*approx_sd
+    left_upper = y_hat - 10*tol
+    right_lower = y_hat + 10*tol
+    right_upper = y_hat + bisectScale*approx_sd
     
     # Left lower bounds
     invalid = (deriv_logdensityratio(left_lower, **dargs) < 0)
@@ -428,7 +428,7 @@ def boundDensityRatio(eta_0, eta_1, mu, sigmasq, yhat, approx_sd, propDf,
     # Return results
     return M
 
-def rcensoredintensities(n_cen, mu, sigmasq, yhat, approx_sd,
+def rcensoredintensities(n_cen, mu, sigmasq, y_hat, approx_sd,
                          p_int_cen, p_rnd_cen,
                          eta_0, eta_1, propDf,
                          tol=1e-10, maxIter=100):
@@ -437,13 +437,13 @@ def rcensoredintensities(n_cen, mu, sigmasq, yhat, approx_sd,
     quantities computed from Laplace approximation
     '''
     # Setup data structures for draws
-    nStates = np.sum(n_cen)
+    n_states = np.sum(n_cen)
     # Intensities
-    intensities = np.zeros(nStates, dtype=np.float64)
+    intensities = np.zeros(n_states, dtype=np.float64)
     # And, the vital indexing vector of length sum(n). This can be used for
     # direct referencing to all input vectors to handle the state to peptide
     # mapping
-    mapping = np.zeros(nStates, dtype=int)
+    mapping = np.zeros(n_states, dtype=int)
     
     # Populate index vector
     filled = 0
@@ -460,13 +460,13 @@ def rcensoredintensities(n_cen, mu, sigmasq, yhat, approx_sd,
     
     # Draw the random censoring indicators. Note that W=1 if randomly censored.
     post_p_rnd_cen = p_rnd_cen / (p_rnd_cen + (1.-p_rnd_cen)*p_int_cen)
-    W = (np.random.uniform(size=nStates) < post_p_rnd_cen[mapping]).astype(int)
+    W = (np.random.uniform(size=n_states) < post_p_rnd_cen[mapping]).astype(int)
     
     # Drawing censored intensities    
     # First, get the maximum of the target / proposal ratio for each set of
     # unique parameter values (not per state)
     M = boundDensityRatio(eta_0=eta_0, eta_1=eta_1, mu=mu, sigmasq=sigmasq,
-                          yhat=yhat, approx_sd=approx_sd,
+                          y_hat=y_hat, approx_sd=approx_sd,
                           normalizing_cnst=1./p_int_cen, propDf=propDf,
                           tol=tol, maxIter=maxIter)
     
@@ -482,7 +482,7 @@ def rcensoredintensities(n_cen, mu, sigmasq, yhat, approx_sd,
         intensities[active] = np.random.standard_t(df=propDf,
                                                    size=np.sum(active))
         intensities[active] *= approx_sd[mapping[active]]
-        intensities[active] += yhat[mapping[active]]
+        intensities[active] += y_hat[mapping[active]]
         
         # Compute acceptance probability
         accept_prob = densityratio(intensities[active],
@@ -490,7 +490,7 @@ def rcensoredintensities(n_cen, mu, sigmasq, yhat, approx_sd,
                                    mu=mu[mapping[active]],
                                    sigmasq=sigmasq[mapping[active]],
                                    approx_sd=approx_sd[mapping[active]],
-                                   yhat=yhat[mapping[active]],
+                                   y_hat=y_hat[mapping[active]],
                                    normalizing_cnst=1./
                                    p_int_cen[mapping[active]],
                                    propDf=propDf, log=False)
