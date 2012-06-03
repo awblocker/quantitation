ca932c1716e83a0efddd6086074fed82d9e20a49
Alexander W Blocker <ablocker@gmail.com>
2012-06-02 22:03:25 -0400

    Add distutils-based setup.py; very bare-bones at this point

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+# Keeping all Python code for package in src directory
+
+setup(name='quantitation',
+      url='http://www.awblocker.com',
+      version='0.1',
+      description='Absolute quantitation for LC/MSMS proteomics via MCMC',
+      author='Alexander W Blocker',
+      author_email='ablocker@gmail.com',
+      packages=['quantitation','quantitation.glm'],
+      package_dir = {'': 'lib'}
+      )
