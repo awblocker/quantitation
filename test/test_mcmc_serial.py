f7d1e22b823dd7f6216abb22697324a9ed99cb91
Alexander W Blocker <ablocker@fas.harvard.edu>
2012-06-06 20:29:37 -0400

    Add memory usage reporting.

--- test/test_mcmc_serial.py
+++ test/test_mcmc_serial.py
@@ -1,3 +1,4 @@
+import os
 import time
 import bz2
 import contextlib
@@ -32,6 +33,32 @@ def effective_sample_size(draws):
     ess = np.shape(draws)[0]*(1.-acf)/(1.+acf)
     return ess
 
+def get_peak_mem():
+    pid = os.getpid()
+    status_file = open("/proc/%d/status" % pid, 'rb')
+    v = status_file.read()
+    status_file.close()
+    i = v.index("VmPeak:")
+    v = v[i:].split(None,3)
+    if len(v) < 3:
+        return '0'
+    else: 
+        return v[1]
+
+def get_current_mem():
+    pid = os.getpid()
+    status_file = open("/proc/%d/status" % pid, 'rb')
+    v = status_file.read()
+    status_file.close()
+    i = v.index("VmSize:")
+    v = v[i:].split(None,3)
+    if len(v) < 3:
+        return '0'
+    else: 
+        return v[1]
+
+print 'Starting memory:', get_current_mem(), get_peak_mem()
+
 # Load config
 cfg = yaml.load(open(path_cfg, 'rb'))
 
@@ -43,6 +70,8 @@ mapping_states_obs, intensities_obs = np.loadtxt(cfg['data']['path_data_state'],
                                                         ('intensity',
                                                          np.float)],
                                                  unpack=True)
+
+print 'Memory after loading data:', get_current_mem(), get_peak_mem()
 
 # Run MCMC sampler
 time_start = time.time()
@@ -56,6 +85,8 @@ print "%f seconds for %d iterations" % (time_done-time_start,
                                         cfg['settings']['n_iterations'])
 print "%f seconds per iteration" % ((time_done-time_start) /
                                     (0.+cfg['settings']['n_iterations']))
+
+print 'Memory after running sampler:', get_current_mem(), get_peak_mem()
 
 # Extract posterior means
 means = {}
@@ -71,6 +102,8 @@ for k, x in draws.iteritems():
 ses = {}
 for k, x in draws.iteritems():
     ses[k] = np.std(x, axis=0)
+
+print 'Memory after computing summaries:', get_current_mem(), get_peak_mem()
 
 if save:
     # Pickle draws and posterior summaries to compressed files
